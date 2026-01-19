01db91bc7b7b55c1ba78d9410591e471333abc70
rodrigo652 <rodrigo.camargo52@outlook.com>
2026-01-18 22:28:02 -0300

    novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { CountDown } from './components/CountDown';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 import './styles/global.css'
 import './styles/theme.css'
 
@@ -8,8 +10,31 @@ import './styles/theme.css'
 export function App() {
     return (
     <>
-        <Container><Heading>LOGO</Heading></Container>
-        <Container><Heading>MENU</Heading></Container>
+        <Container><Logo/></Container>
+        <Container><Menu/></Container>
+        <Container><CountDown/></Container>
+        <Container>
+            <form className='form' action="">
+                <div className='formRow'>
+                    <label htmlFor='input'>task</label>
+                    <input id='input' type="text" />
+                </div>
+
+                <div className='formRow'>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+
+                <div className='formRow'>
+                    <p>Ciclos</p>
+                    <p>0 0 0 0 0</p>
+                </div>
+
+                <div className='formRow'>
+                    <button>Enviar</button>
+                </div>
+
+            </form>
+        </Container>
         
     </>
     );
