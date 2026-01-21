c16d063b27bca2481dea2bc38d9a06a51633a6f3
ebravo <contato@ebravo.com.br-at-877432383412>
2026-01-21 17:24:14 -0300

    novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,10 @@
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { DefaultInput } from './components/DefaultInput';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
 import './styles/global.css'
 import './styles/theme.css'
-
+import { Home } from './pages/Home';
 
 
 export function App() {
-    return (
-    <>
-        <Container><Logo/></Container>
-        <Container><Menu/></Container>
-        <Container><CountDown/></Container>
-        <Container>
-            <form className='form' action="">
-                <div className='formRow'>
-                    <DefaultInput 
-                        labelText='task'
-                        id='meuInput' 
-                        type='text'
-                        placeholder='Digite algo'/>
-                </div>
-
-                <div className='formRow'>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </div>
-
-                <div className='formRow'>
-                    <Cycles/>
-                </div>
-
-                <div className='formRow'>
-                    <button>Enviar</button>
-                </div>
-
-            </form>
-        </Container>
-        
-    </>
-    );
+    return <Home />
 }
+
 
