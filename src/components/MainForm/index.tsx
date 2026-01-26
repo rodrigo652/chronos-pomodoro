87de9323f087b26e6481fa492412b0ee425c353a
ebravo <contato@ebravo.com.br-at-877432383412>
2026-01-26 15:40:27 -0300

    utils

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,14 +2,57 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
-import { useState } from "react";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecodsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  const {state, setState} = useTaskContext()
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    
+    if(taskNameInput.current === null) return;
+    
+    const taskName = taskNameInput.current.value.trim();
+
+    if(!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {...prevState.config},
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecodsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
+
   }
 
   return (
@@ -20,8 +63,7 @@ export function MainForm() {
           id='meuInput'
           type='text'
           placeholder='Digite algo'
-          value={taskName}
-          onChange={e => setTaskName(e.target.value)}
+          ref={taskNameInput}
         />
       </div>
 
